52f30e7a65d3026f4e2a8592da6037ef5bf85ef6
Denis <denis.yaremenko@huma.com>
2024-04-22 10:40:11 +0300

    Initial Commit

---
+++ SwiftDataMovies/SwiftDataMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataMoviesApp.swift
+//  SwiftDataMovies
+//
+//  Created by Denis Yaremenko on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
